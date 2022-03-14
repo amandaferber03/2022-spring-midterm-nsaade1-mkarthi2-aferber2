866621482d424405c6a75a8c6e845211422e3b30
nicolesaade <nicolesaadea@gmail.com>
2022-03-13 21:51:09 -0400

    Worked on Sythesize from exemplar helper functions. Nicole, Meghana and Amanda were all drivers.

--- texture_synthesis.c
+++ texture_synthesis.c
@@ -7,69 +7,184 @@
 #include <assert.h>
 #include "texture_synthesis.h"
 
-Image * placeImage(int width, int height, const Image * image) {
+TBSPixel * count_neighbors(Image * img, const Image * exemplar) {
+	int width = img->width;
+	int height = img->height;
+	int exemplarProd = (exemplar->width)*(exemplar->height);
+	int imgProd = (img->width)*(img->height);
+	int pixelAmount = imgProd - exemplarProd;
+ 
+	// variables to represent alpha values of neighboring pixels
+	
+	Pixel * pixel = img->pixels;
+	TBSPixel * tbs_pixels = (TBSPixel *) malloc(sizeof(TBSPixel) * pixelAmount);
+	int tbs_counter = 0;
+	for (int i = 0; i < (width * height); i++) {
+		int tbs_neighbor_tracker = 0;
+		int unset_pixel_x = i % width;
+		int unset_pixel_y = i / width;
+
+		// finding neighbor counts of unset pixels
+		if (pixel[i].a == 0) {
+			if ((i+width) >= (width * height)) { // BOTTOM-MOST PIXELS
+				if ((i+1) == (width * height)) { // BOTTOM RIGHT
+					if (pixel[i-width].a == 255) { // up
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i - width - 1].a == 255) { // top-left
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i - 1].a == 255) { // left
+						tbs_neighbor_tracker++;
+					}
+				}
+				else if (i == ((width * height) - (width - 1))) { // BOTTOM LEFT
+					if (pixel[i - width].a == 255) { // up
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i - width + 1].a == 255) { // top-right
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i + 1].a == 255) { // right
+						tbs_neighbor_tracker++;
+					}
+					}
+				}
+				else { // BOTTOM
+					if (pixel[i-width].a == 255) { // top
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i - width + 1].a == 255) { // top-right
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i - width - 1].a == 255) { // top-left
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i - 1].a == 255) { // left
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i + 1].a == 255) { // right
+						tbs_neighbor_tracker++;
+					}
+				}
+			}
+			else if ((i % width) == 0) { // LEFT-MOST PIXELS
+				if (pixel[i-width].a == 255) { // up
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i - width + 1].a == 255) { // top-right
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i + 1].a == 255) { // right
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i + width].a == 255) { // bottom
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i + width + 1].a == 255) { // bottom-right
+					tbs_neighbor_tracker++;
+				}
+
+			}
+			else if (((i + 1) % width) == 0) { // RIGHT-MOST PIXELS
+				if (i + 1 == width) { // TOP RIGHT
+					if (pixel[i - 1].a == 255) { //left
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i + width].a == 255) { //bottom
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i + width - 1].a == 255) { //bottom-left
+						tbs_neighbor_tracker++;
+					}
+				}
+				else { // RIGHT
+					if (pixel[i-width].a == 255) { // up
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i - width - 1].a == 255) { // top-left
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i - 1].a == 255) { // left
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i + width].a == 255) { // bottom
+						tbs_neighbor_tracker++;
+					}
+					if (pixel[i + width - 1].a == 255) { //bottom-left
+						tbs_neighbor_tracker++;
+					}
+				}
+			}
+			else if ((i < width) == 0) { // TOP
+				if (pixel[i - 1].a == 255) { //left
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i + 1].a == 255) { //right
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i + width].a == 255) { //bottom
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i + width - 1].a == 255) { //bottom-left
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i + width + 1].a == 255) { //bottom-right
+					tbs_neighbor_tracker++;
+				}
+			}
+			else { // none of the borders
+				if (pixel[i-width].a == 255) { //up
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i - width + 1].a == 255) { //top-right
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i - width - 1].a == 255) { //top-left
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i - 1].a == 255) { //left
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i + 1].a == 255) { //right
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i + width].a == 255) { //bottom
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i + width - 1].a == 255) { //bottom-left
+					tbs_neighbor_tracker++;
+				}
+				if (pixel[i + width + 1].a == 255) { //bottom-right
+					tbs_neighbor_tracker++;
+				}
+			}
+
+		// setting idx and neighborCount fields
+		tbs_pixels[tbs_counter].idx.x = unset_pixel_x;
+		tbs_pixels[tbs_counter].idx.y = unset_pixel_y;
+
+		// next index in TBS pixel array
+		tbs_counter++;
+	}
+	return tbs_pixels;
+}
+
+
+Image * place_image(int width, int height, const Image * image) {
   if ((width < image->width) || (height < image->height)) {
     return NULL;
   }
   
   Image * img = (Image *) malloc(sizeof(Image));
-  
   img->width = width;
-  
   img->height = height;
-
-  
   img->pixels = (Pixel *) malloc(sizeof(Pixel) * width * height);
 
   Pixel * pixels = img->pixels;
   Pixel * exemplar_pixels = image->pixels;
   int count = 0;
   int counter = 0; 
-  
-  /*for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-      int position = (width * i) + j; 
-      if (i <= 50 && j <= 66) {
-		(pixels[position]).r = (exemplar_pixels[position]).r;
-		(pixels[position]).g = (exemplar_pixels[position]).g;
-		(pixels[position]).b = (exemplar_pixels[position]).b;
-		(pixels[position]).a = 255;
-      }
-      else {
-		(pixels[position]).r = 0;
-        (pixels[position]).g = 0;
-        (pixels[position]).b = 0;
-		(pixels[position]).a = 0;
-      }
-    }
-  } */
-
-//printf("Hey\n");
-  
-  /*for (int i = 0; i < (img->width * img->height); i++) {
-	  Pixel * pixels = img->pixels;
-      Pixel * exemplar_pixels = image->pixels;
-	  //printf("Hey1\n");
-	  if ((i / width) % (height-1) < img->height) { //Evaluating we are in an applicable row
-		  if ((i % (width - 1) <= img->width) ) { //Evaluating if we are in an applicable column
-			(pixels[i]).r = (exemplar_pixels[i]).r;
-			(pixels[i]).g = (exemplar_pixels[i]).g;
-			(pixels[i]).b = (exemplar_pixels[i]).b;
-			(pixels[i]).a = 255;
-		  }
-		  //printf("Hey2\n");
-		  //printf("%d %d %d", width, height, width * height);
-		  //printf("%d\n", i);
-	  }
-	  
-	  else {
-		(pixels[i]).r = 0;
-        (pixels[i]).g = 0;
-        (pixels[i]).b = 0;
-		(pixels[i]).a = 0;
-		//printf("Hey3\n");
-	  }
-  } */
 
   for(int i = 0; i < width * height; i++) {
 	  if(i >= (count*width) && i <= (count*width + image->width -1) && !(count >= image->height)) {
@@ -94,7 +209,6 @@ Image * placeImage(int width, int height, const Image * image) {
   }
   return img;
   
-    
 }
 // compares tbs pixels 
 int CompareTBSPixels( const void *v1 , const void *v2 )
@@ -139,10 +253,26 @@ int SortTBSPixels( TBSPixel *tbsPixels , unsigned int sz )
 }
 
 
-Image *SynthesizeFromExemplar( const Image *exemplar , unsigned int outWidth , unsigned int outHeight , unsigned int windowRadius , bool verbose )
+Image *SynthesizeFromExemplar( const Image *exemplar , unsigned int outWidth , unsigned int outHeight , unsigned int windowRadius) // bool verbose
 {
-	Image *synthesized = NULL;
+	printf("Hello");
+	Image * new_image = place_image(outWidth, outHeight, exemplar);
+	printf("Hello");
+	TBSPixel * neighbor_counts = count_neighbors(new_image, exemplar);
+	
+	// SortTBSPixels successful
+	int exemplar_pixels = exemplar->width * exemplar->height;
+	int new_pixels = new_image->width * new_image->height;
+	int TBSPixel_arr_size = exemplar_pixels * new_pixels;
+
+	printf("Hello");
+	if (SortTBSPixels(neighbor_counts, TBSPixel_arr_size) == 0) {
+		printf("Success!\n");
+	}
+	else {
+		printf("Failure :(\n");
+	}
 
 	// TODO: IMPLEMENT THIS FUNCTION
-	return synthesized;
+	return new_image;
 }
